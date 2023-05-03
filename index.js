b5845dba211bf980adc6533f579b6387ccb73e7d
unknown <supergamer9012@gmail.com>
2023-05-03 16:52:10 +0300

    Add routing

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express=require('express')
 const path=require('path')
 const exphbs=require('express-handlebars')
+const homeRoutes=require('./routes/home')
+const addRoutes=require('./routes/add')
+const coursesRoutes=require('./routes/courses')
 
 const app=express()
 
@@ -15,24 +18,9 @@ app.set('views','views')
 
 app.use(express.static('public'))
 
-app.get('/', (req,res)=>{
-    res.render('index',{
-        title: 'Главная страница',
-        isHome: true
-    })
-})
-app.get('/add',(req,res)=>{
-    res.render('add',{
-        title: 'Добавить курс',
-        isAdd: true
-    })
-})
-app.get('/courses',(req,res)=>{
-    res.render('courses',{
-        title: 'Курсы',
-        isAdd: true
-    })
-})
+app.use('/',homeRoutes)
+app.use('/add',addRoutes)
+app.use('/courses',coursesRoutes)
 
 const PORT=process.env.PORT || 3000
 
